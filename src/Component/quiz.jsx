497de1ce64c9200bc89b6027038fed46f0cd3c3c
vimal211 <vimalnavee2110@gmail.com>
2021-06-24 21:51:32 +0530

    Basic Structure 2

--- src/Component/quiz.jsx
+++ src/Component/quiz.jsx
@@ -7,7 +7,7 @@ function Quiz() {
             <div className="question">What is the best Battle Royal game?</div>
             <div className="answers">
                 <div className="answer">PUBG</div>
-                <div className="answer correct">Fortnite</div>
+                <div className="answer wrong">Fortnite</div>
                 <div className="answer">Apex Legends</div>
                 <div className="answer">Battlefield</div>
             </div>
